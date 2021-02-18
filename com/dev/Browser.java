166f8c747e628f8890682428d4486aa7b08dbfd5
Sophia Jung <sophiabraddock82@gmail.com>
2021-02-18 16:37:37 -0500

    'browser'

---
+++ com/dev/Browser.java
@@ -0,0 +1,17 @@
+package com.dev;
+
+public class Browser {
+    public void navigate(String address) {
+        String ip = findIpAddress(address);
+        String html = sentHttpRequest(ip);
+        System.out.println(html);
+    }
+
+    private String sentHttpRequest(String ip) {
+        return "<html><html>";
+    }
+
+    private String findIpAddress(String address) {
+        return "127.0.0.0.1";
+    }
+}
